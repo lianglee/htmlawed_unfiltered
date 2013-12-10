745388f67e3a52b955d27626abdc936dd583076d
Liang Lee <manactric@gmail.com>
2013-12-10 23:10:54 +0500

    Update start.php

--- start.php
+++ start.php
@@ -1,8 +1,9 @@
 <?php
 elgg_register_plugin_hook_handler('config', 'htmlawed', 'htmlawed_unfiltered');
 function htmlawed_unfiltered($hook, $type, $result, $params){
-	if(elgg_in_context('blog')){
+     //if you want to allow all then remove line number 5 and 8
+	if(elgg_is_admin_logged_in()){
 	$config = array('iframe' => 1, 'script' => 1); 
-    return htmLawed($text, $config); 
+        return htmLawed($text, $config);
 	}
 }
